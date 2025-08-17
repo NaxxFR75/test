edf2bfb4cda2459c06d629a3f875881a8a65d290
NaxxFR75 <n4sm3n@gmail.com>
2025-08-17 04:45:50 +0200

    Add files via upload

---
+++ hacking/html/src/language.js
@@ -0,0 +1,30 @@
+export default
+{
+    // langue française = 'FR'
+    // lengua española = 'ES'
+    // lingua italiana = 'IT'
+    // english language = 'EN'
+    // polska gurom = 'PL'
+    // german language = 'DE'
+    SELECTED_LANGUAGE : 'FR',
+
+    LANGUAGES : ['PL', 'EN', 'ES', 'IT', 'FR', 'DE'],
+    PL : {
+        COLORS : ['czarny', 'bialy', 'niebieski', 'czerwony', 'zolty', 'pomaranczowy', 'zielony', 'fioletowy']
+    },
+    EN : {
+        COLORS : ['black', 'white', 'blue', 'red', 'yellow', 'orange', 'green', 'purple']
+    },
+    ES : {
+        COLORS : ['negro', 'blanco', 'azul', 'rojo', 'amarillo', 'naranja', 'verde', 'púrpura']
+    },
+    IT : {
+        COLORS : ['nero', 'bianco', 'blu', 'rosso', 'giallo', 'arancione', 'verde', 'viola']
+    },
+    FR : {
+        COLORS : ['noir', 'blanc', 'bleu', 'rouge', 'jaune', 'orange', 'vert', 'violet']
+    },
+    DE : {
+        COLORS : ['schwarz', 'weiß', 'blau', 'rot', 'gelb', 'orange', 'grün', 'lila']
+    }
+}
